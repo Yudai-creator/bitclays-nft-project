be0de1c5b84664dbf62508fddb96764ba5d5a8f9
Yudai-creator <yudaipx77@gmail.com>
2022-09-23 15:00:55 -0400

    fix:import fonts with global css

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AppButton from '../components/AppButton'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
