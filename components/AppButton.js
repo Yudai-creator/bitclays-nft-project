2fa9d885749495f474bca7cffb31134780e53cb1
Yudai-creator <yudaipx77@gmail.com>
2022-09-30 15:45:58 -0400

    fix:target _blank issue

--- components/AppButton.js
+++ components/AppButton.js
@@ -1,7 +1,7 @@
 const AppButton = () => {
     return ( 
         <>
-            <a className="p-4  bg-gradient-to-br from-gradient_1 to-accent rounded" href="https://discord.gg/nz4CN2kg" target="_blank">Join our community</a>   
+            <a className="p-4  bg-gradient-to-br from-gradient_1 to-accent rounded" href="https://discord.gg/nz4CN2kg" target="_blank" rel="noreferrer">Join our community</a>   
         </>
      );
 }
